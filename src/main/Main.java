e47b345671ea8110bf45173a32896c6697aefdf3
Sourabhvk <sourabhkolekar16@gmail.com>
2020-02-12 22:34:56 +1100

    Added JavaDocs, Compiled executable Jar, modify for system console output instead of IDE console

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,24 @@
 package main;
 
 import main.infrastructure.UserInterface;
+
 import java.io.Console;
+
+import static main.domain.common.commonObjects.CONSOLE_NOT_AVAILABLE_FOR_THE_EXECUTION;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //check if system console is available
+        // check if system console is available
         Console console = System.console();
-        //if console is not available or project is executed in IDE the show console not available
-//        if (console == null) {
-//            System.err.println(CONSOLE_NOT_AVAILABLE_FOR_THE_EXECUTION);
-//            System.exit(1);
-//        }
-//        else
-//        {
-        UserInterface userInterface = new UserInterface();
-        userInterface.start(console);
-//        }
+        // if console is not available or project is executed in IDE the show console not available
+        if (console == null) {
+            System.err.println(CONSOLE_NOT_AVAILABLE_FOR_THE_EXECUTION);
+            System.exit(1);
+        } else {
+            UserInterface userInterface = new UserInterface();
+            userInterface.start(console);
+        }
     }
 }
--- src/main/infrastructure/UserInterface.java
+++ src/main/infrastructure/UserInterface.java
@@ -10,13 +10,12 @@ public class UserInterface {
         System.out.println("----------------------------------------------------------------------------");
         System.out.println("Robo Demo");
         System.out.println("Enter a command, Valid commands are:");
-        System.out.println("\'PLACE X,Y,NORTH|SOUTH|EAST|WEST\', MOVE, LEFT, RIGHT, REPORT or FINISH/EXIT/END");
+        System.out.println("'PLACE X,Y,NORTH|SOUTH|EAST|WEST', MOVE, LEFT, RIGHT, REPORT or FINISH/EXIT/END");
 
         RoboController controller = new RoboController();
         while (true) {
-            // TODO: 12/02/2020 console work
-//            String input = console.readLine(": ");
-            String input = IOUtility.getString("-> ").toUpperCase();
+            String input = console.readLine("-> ").toUpperCase();
+//            String input = IOUtility.getString("-> ").toUpperCase();
             switch (input) {
                 case "FINISH":
                 case "EXIT":
